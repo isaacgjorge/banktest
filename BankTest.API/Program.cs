7726e3419f4319f2324a17c0e8da4384ccaf8fa8
isaacgjorge <isaac.g.jorge@gmail.com>
2022-09-25 22:54:08 +0200

    bugs and improvements

--- BankTest.API/Program.cs
+++ BankTest.API/Program.cs
@@ -1,31 +1,35 @@
 using API;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddServices();
-
-builder.Services.AddAuthorization();
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+static async Task Main(string[] args)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+
+    builder.Services.AddServices();
+
+    builder.Services.AddAuthorization();
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ WebApi/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿
+using Domain.Data;
+using Domain.Repository;
+using Domain.Repository.Interfaces;
+using Services;
+using Services.Interfaces;
+
+namespace API;
+
+public static class DependencyInjection
+{
+    public static void AddServices(this IServiceCollection services)
+    {
+        services.AddDbContext<ApplicationContext>();
+        services.AddTransient<IUserService, UserService>();
+        services.AddTransient<IAccountService, AccountService>();
+        services.AddTransient<IUserRepository,
+            UserRepository>();
+        services.AddTransient<IAccountRepository,
+            AccountRepository>();
+    }
+}
+
